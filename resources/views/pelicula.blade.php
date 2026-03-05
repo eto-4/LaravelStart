d2252ed24c7d77cb0f8760574d154e47f9545e86
usuario <aleix.murt22@lacetania.cat>
2026-03-05 20:26:03 +0100

    Part_4_T2: Completat (Canvi estructural en els views amb herència estructural)

--- resources/views/pelicula.blade.php
+++ resources/views/pelicula.blade.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-    <html>
-    <head>
-        <title>Laravel</title>
-    </head>
-    <body>
-        <h1>Pàgina de pelicules!</h1>
-        <h1> {{ $titol }} </h1>
-        <h2> {{ $data }} </h2>
-    </body>
-</html>
-
 @extends('layouts.app')
 
 @section('title', 'Películes')
