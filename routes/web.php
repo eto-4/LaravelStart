1353b0af229be60668e801e95d84e32ade42f305
Aleix <aleix.murt22@lacetania.cat>
2026-03-05 16:37:39 +0100

    Part_2: Completada (Ús de variables)

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $hola = "Hola, ";
+    $mon = "món!";
+    return view('welcome', compact('hola', 'mon'));
 });
