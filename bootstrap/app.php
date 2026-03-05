7cb5abac745b0674933cc4d1859925873a96f462
usuario <aleix.murt22@lacetania.cat>
2026-03-05 19:16:50 +0100

    Part_10: Completada (Captura d'errors, configuració captura de errors 404, i vista 404)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        // Pagina 404
+        $exceptions->render(
+            function (NotfountHttpException $e, Request $request) {
+                return response()->view('errors.404', [], 404);
+            });
     })->create();
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <title>Pàgina no trobada</title>
+</head>
+<body>
+    <h1>404 - Pàgina no trobada</h1>
+    <p>La pàgina que estàs buscant no existeix.</p>
+    <a href="{{ route('home') }}">Torna a l'inici</a>
+</body>
+</html>
